c938269f36eae2f454b90281f826c8df99b2425c
Abner Galvez Contreras <agalvez@camelsecure.com>
2023-06-12 13:43:39 -0400

    feat[view]: The view index is created for instructions

---
+++ resources/views/index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>⚡ Descending Prime Numbers API</title>
+    <style>
+        table, th, td {
+            border-collapse: collapse;
+            border: 0.5px solid black;
+            padding: .25rem .75rem;
+        }
+
+        th {
+            border-top: .375rem;
+            background-color: lightgrey;
+        }
+
+        body{
+            width: 60%;
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 10px;
+            padding-left: 20px;
+            border-left: 1px dotted gray;
+            padding-right: 20px;
+            border-right: 1px dotted gray;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    echo $indexView;
+    ?>
+</body>
+</html>
+
+
+
